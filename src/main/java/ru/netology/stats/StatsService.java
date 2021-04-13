c1f275d4c9d5ccc5f62bb020e69d9b3dbb7feadd
Yulia Kobeleva <kobelevaulia@gmail.com>
2021-04-14 00:15:17 +0300

    commit

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,22 +1,65 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public long calculateSum(long[] purchases) {
-        long sum = 0;
-        for (long purchase : purchases) {
+    public long calculateSum(long[] months) {
+        long sum = 0; // начинаем с нуля
+        for (long month : months) {
             // аналог sum = sum + purchase;
-            sum += purchase;
+            sum += month;
         }
         return sum;
     }
 
-    public long findMax(long[] purchases) {
-        long currentMax = purchases[0];
-        for (long purchase : purchases) {
-            if (currentMax < purchase) {
-                currentMax = purchase;
+    public long findMidle(long[] midlesums) {
+        long currentMidle = midlesums[0];
+        for (long midlesum : midlesums) {
+            if (currentMidle == midlesum) {
+                currentMidle = midlesum;
             }
         }
+        return currentMidle;
+    }
+
+    public int maxValue(int[] values) {
+        int currentMax = values[0]; // берём за точку отсчёта первый
+        for (int value : values) {
+            // на каждой итерации прибавляем единицу
+            // таким образом для первого значения month = 1
+            if (currentMax > value) {
+                currentMax = value;
+            }
+          }
         return currentMax;
     }
-}
+
+        public int minValue (int[] values){
+            int currentMin = values[0]; // берём за точку отсчёта первый
+            for (int value : values) {
+                // на каждой итерации прибавляем единицу
+                // таким образом для первого значения month = 1
+                if (currentMin < value) {
+                    currentMin = value;
+                }
+                 }
+            return currentMin;
+        }
+
+    public long findBelowTheMidle(long[] midlesums) {
+        long currentMidle = midlesums[0]; // берём за точку отсчёта первый
+        for (long midlesum : midlesums) {
+            if (currentMidle < midlesum) {
+                currentMidle = midlesum;
+            }
+        }
+        return currentMidle;
+    }
+    public long findAboveMidle(long[] midlesums) {
+        long currentMidle = midlesums[0]; // берём за точку отсчёта первый
+        for (long midlesum : midlesums) {
+            if (currentMidle < midlesum) {
+                currentMidle = midlesum;
+            }
+        }
+        return currentMidle;
+    }
+    }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -4,27 +4,75 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StatisticsServiceTest {
+class StatsServiceTest {
     @Test
     void calculateSum() {
-        StatisticsService service = new StatisticsService();
+        StatsService service = new StatsService();
 
-        long[] purchases = {1_000, 2_000, 500, 5_000, 2_000};
-        long expected = 10_500;
+        long[] months = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
 
-        long actual = service.calculateSum(purchases);
+        long actual= service.calculateSum(months);
 
         assertEquals(expected, actual);
     }
 
     @Test
-    void findMax() {
-        StatisticsService service = new StatisticsService();
+    void findMidle() {
+        StatsService service = new StatsService();
 
-        long[] purchases = {1_000, 2_000, 500, 5_000, 2_000};
-        long expected = 5_000;
+        long[] midlesums = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 20;
 
-        long actual = service.findMax(purchases);
+        long actual = service.findMidle(midlesums);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void maxValue() {
+        StatsService service = new StatsService();
+
+        long[] currentMax = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 6;
+
+        long actual = service.findMidle(currentMax);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void minValue() {
+        StatsService service = new StatsService();
+
+        long[] currentMin = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+
+        long actual = service.findMidle(currentMin);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findBelowTheMidle() {
+        StatsService service = new StatsService();
+
+        long[] currentMidle = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 2;
+
+        long actual = service.findMidle(currentMidle);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAboveMidle() {
+        StatsService service = new StatsService();
+
+        long[] currentMidle = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 6;
+
+        long actual = service.findMidle(currentMidle);
 
         assertEquals(expected, actual);
     }
